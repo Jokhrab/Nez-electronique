b11d8a4d57740793f62254522ceef1af93027ff7
Jokhrab <130192240+Jokhrab@users.noreply.github.com>
2023-04-08 01:53:36 +0100

    Add files via upload

---
+++ nez electronique.py.py
@@ -0,0 +1,150 @@
+Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+import time
+import RPi.GPIO as GPIO
+#import serial
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+
+'''
+define pin for lcd
+'''
+# Timing constants
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+delay = 1
+buzzer=37
+GPIO.setup(buzzer, GPIO.OUT)  
+
+# Define GPIO to LCD mapping
+LCD_RS = 7
+LCD_E  = 11
+LCD_D4 = 12
+LCD_D5 = 13
+LCD_D6 = 15
+LCD_D7 = 16
+# connect perp
+gas_Sensor = 18
+red_light = 31
+Buzzer= 29
+# IN OUTput
+GPIO.setup(LCD_E, GPIO.OUT)  
+GPIO.setup(LCD_RS, GPIO.OUT) 
+GPIO.setup(LCD_D4, GPIO.OUT) 
+GPIO.setup(LCD_D5, GPIO.OUT) 
+GPIO.setup(LCD_D6, GPIO.OUT) 
+
+GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+GPIO.setup(gas_Sensor, GPIO.IN) # DB7
+GPIO.setup(red_light, GPIO.OUT)
+GPIO.setup(Buzzer, GPIO.OUT)
+# Define some device constants
+LCD_WIDTH = 16    # Maximum characters per line
+LCD_CHR = True
+LCD_CMD = False
+LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+
+'''
+Function Name :lcd_init()
+Function Description : this function is used to initialized lcd by sending the different commands
+'''
+def lcd_init():
+  # Initialise display
+  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
+  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+  time.sleep(E_DELAY)
+'''
+Function Name :lcd_byte(bits ,mode)
+Fuction Name :the main purpose of this function to convert the byte data into bit and send to lcd port
+'''
+def lcd_byte(bits, mode):
+  # Send byte to data pins
+  # bits = data
+  # mode = True  for character
+  #        False for command
+ 
+  GPIO.output(LCD_RS, mode) # RS
+ 
+  # High bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x10==0x10:
+    GPIO.output(LCD_D4, True)
+  if bits&0x20==0x20:
+    GPIO.output(LCD_D5, True)
+  if bits&0x40==0x40:
+    GPIO.output(LCD_D6, True)
+  if bits&0x80==0x80:
+    GPIO.output(LCD_D7, True)
+ 
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+ 
+  # Low bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x01==0x01:
+    GPIO.output(LCD_D4, True)
+  if bits&0x02==0x02:
+    GPIO.output(LCD_D5, True)
+  if bits&0x04==0x04:
+    GPIO.output(LCD_D6, True)
+  if bits&0x08==0x08:
+    GPIO.output(LCD_D7, True)
+ 
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+'''
+Function Name : lcd_toggle_enable()
+Function Description:basically this is used to toggle Enable pin
+'''
+def lcd_toggle_enable():
+  # Toggle enable
+  time.sleep(E_DELAY)
+  GPIO.output(LCD_E, True)
+  time.sleep(E_PULSE)
+  GPIO.output(LCD_E, False)
+  time.sleep(E_DELAY)
+'''
+Function Name :lcd_string(message,line)
+Function  Description :print the data on lcd 
+'''
+def lcd_string(message,line):
+  # Send string to display
+ 
+  message = message.ljust(LCD_WIDTH," ")
+ 
+  lcd_byte(line, LCD_CMD)
+ 
+  for i in range(LCD_WIDTH):
+    lcd_byte(ord(message[i]),LCD_CHR)
+
+lcd_init()
+lcd_string("NEZ ELECTRONIQUE  ",LCD_LINE_1)
+time.sleep(1)
+# Define delay between readings
+delay = 5
+
+while 1:
+  # Print out results
+  if GPIO.input(gas_Sensor):
+   lcd_string(" Gas Detected  ",LCD_LINE_2)
+   GPIO.output(Buzzer, True)
+   GPIO.output(red_light, True)
+  else:
+    lcd_string("Gas Not Detected  ",LCD_LINE_2)
+    GPIO.output(Buzzer, False)
+    GPIO.output(red_light, False)
+
+  
