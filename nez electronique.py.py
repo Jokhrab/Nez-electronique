f1ffc43f43b24ed4ba8242ddc7ce0aef73bed4a6
Jokhrab <130192240+Jokhrab@users.noreply.github.com>
2023-04-08 02:54:05 +0200

    Update nez electronique.py.py

--- nez electronique.py.py
+++ nez electronique.py.py
@@ -1,6 +1,4 @@
-Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
+
 import time
 import RPi.GPIO as GPIO
 #import serial
